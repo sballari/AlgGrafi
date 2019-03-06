120bd31d953997b908ffcbf685fddffcb17da9ed
sballari <ballarinsimone@gmail.com>
2019-03-06 13:46:55 +0100

    dpa

--- DPATrial.py
+++ DPATrial.py
@@ -1,15 +1,41 @@
 import random
+import graph
 
-class DPATrial:
-    def __init__(self,nodes):
-        self.num=len(nodes)
-        self.nodeNumbers=[]
-        for i in range(self.num):
-            self.nodeNumbers+=[i for i in nodes]
-    def RunTrial(self,node_label):
-        Vi=[]
-        for i in range(self.num):
-            Vi.append(random.choice(self.nodeNumbers))
-        self.nodeNumbers.append(node_label)
-        self.nodeNumbers+=Vi
-        return Vi
+
+# nodes : urna iniziale 
+# setta l'urna con m volte ogni nodo
+def DPATrial(nodes):
+    V=[]
+    for i in range(len(nodes)-1): #len corrisponde a m
+        V+=nodes
+    return V
+
+# urn : urna
+# nodeLabel : nodo che esegue la pescata
+# m : numero di estrazioni da fare
+def RunTrial(urn,node_label,m): #estrazione di nodeLabel
+    Vi=[] #nodi pescati
+    for i in range(m):
+        Vi.append(random.choice(urn))
+    urn.append(node_label) #biglietto bianco
+    urn+=Vi #amplio l'urna con i cloni, biglietti gialli
+    return Vi
+
+
+def DpaGraph(m,nodesL):
+    G=graph.Graph() #empty own graph
+    for i in nodesL:
+        G.addNode(graph.Node(i))
+
+    urn_nodes=[]  #scelta urna iniziale da nodi iniziali
+    for i in range(m):
+        label=random.choice([i for i in G.nodes])
+        urn_nodes.append(label)
+
+    urn_nodes =DPATrial(urn_nodes)
+
+    for node1 in G.nodes:
+        Vi= RunTrial(urn_nodes,node1,m)
+        for node2 in Vi: #man mano che pesco creo il grafo
+            G.addEdge(node1,node2)
+    return G
--- alg2.py
+++ alg2.py
@@ -2,22 +2,6 @@ import graph
 import random
 import DPATrial as DPA
 
-G=graph.Graph() #empty own graph
-for i in range(20):
-    G.addNode(graph.Node(i))
-
-n=1
-
-initial_nodes=[]
-for i in range(n):
-    label=random.choice([i for i in G.nodes])
-    initial_nodes.append(label)
-
-dpa = DPA.DPATrial(initial_nodes)
-
-for node1 in G.nodes:
-    Vi=dpa.RunTrial(node1)
-    for node2 in Vi:
-        G.addEdge(node1,node2)
-
+m=1
+G=DPA.DpaGraph(m,[i for i in range(20)])
 G.plotGraph()
