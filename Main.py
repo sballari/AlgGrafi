697be95cd589a9002f1a3ae63207a2650bb3fbca
sballari <ballarinsimone@gmail.com>
2019-03-08 18:08:38 +0100

    utility

--- Main.py
+++ Main.py
@@ -12,17 +12,16 @@ import matplotlib.pyplot as plt
 import sys
 
 sys.setrecursionlimit(10000)
-#G =IOG.IOGraph ('as20000102.txt', False)
-#G = ER.ERNotOrientedGraph(rangeL, 0.0003)
+# G =IOG.IOGraph ('as20000102.txt', False)
+# G = ER.ERNotOrientedGraph(rangeL, 0.0003)
 rangeL = [i for i in range(6474)]
-G = DPA.DpaGraph(1,rangeL)
-
+G = DPA.DpaGraph(2,rangeL)
+ 
 avg = str(G.avg_degree())
 print "degree IOG: "+str(avg)
-
+print 'archi:', G.getEdgeNumber()
 #print "fine generazione [0..6474]"
 # print "fine ER"
-
 
 dim_CC_maxs=[] #dimensione della componente connessa massima al variare del tempo
 tm=time.time()
@@ -31,7 +30,6 @@ while len(G.nodesLabel) != 0:
     nodeOff.MaxDegreeOffNode(G)
     arrayCC=CC.ConnectedComponets(G)
     dim_CC_maxs.append(CC.dim_CC_max(arrayCC))
-    print len(G.nodesLabel)
 print time.time()-tm
 
 df=pd.DataFrame(data={'dim_CC_maxs': dim_CC_maxs,'index': [i for i in range(len(dim_CC_maxs))]})
