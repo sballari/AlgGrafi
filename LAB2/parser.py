d5595b7df3840d04a6cd7441d7223f35c3fc22ab
daniel <bovo.96@gmail.com>
2019-03-19 23:53:07 +0100

    file parsati

--- LAB2/parser.py
+++ LAB2/parser.py
@@ -5,12 +5,12 @@ import string
 
 mezzi = set()
 corse = set()
-
+fermate = set()
+stazioni = set()
+passaggio = []
 
 mypath="/home/rossidaniel/Documenti/data_transport/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-print onlyfiles
 
 for t_file in onlyfiles:
     f=file(str(mypath)+t_file)
@@ -18,21 +18,57 @@ for t_file in onlyfiles:
     s=''.join(x if x in string.printable else " " for x in s)
     s=s.split('*Z ')
     s.pop(0)
-    for first_run in s:
-        #print first_run
-        first_run = first_run.split("\n")
-        ciao= first_run.pop(0).split(" ")
-        ciao= filter(lambda a: a != "", ciao)
-        ciao[1]=ciao[1].replace("-","")
-        corse.add(ciao[0])
-        mezzi.add(ciao[1])
-
+    for trip in s:
+        lines = trip.split("\n")
         
-        #corsa=intestazione[0]
-        #print corsa
-    #print "-----------------------------------"
+
+
+        first_line= lines.pop(0).split(" ")[:2]
+        second_line=lines[0].split(" ")[:2]
+        first_line[1]=first_line[1].replace("-","")+second_line[1]
+
+        corse.add(first_line[0])
+        mezzi.add(first_line[1])
+
+        while lines[0].split(" ")[0][0]=='*':
+            lines.pop(0)
+        for line in lines:
+            fermata=line[0:9]
+            stazione=line[10:30]
+            start=line[32:37]
+            finish=line[39:44]
+            
+            if fermata != '':
+                fermate.add(fermata)
+                stazioni.add(stazione)
+                passaggio.append([fermata,stazione,start,finish])      
+
+        corse.add(first_line[0])
+        mezzi.add(first_line[1])
+
     f.close()
 
-for i in mezzi:
+for i in passaggio:
     print i
-print corse
+
+f=open("mezzi.csv","w")
+for i in mezzi:
+    f.write(i+"\n")
+
+f=open("corse.csv","w")
+for i in corse:
+    f.write(i+"\n")
+
+f=open("fermate.csv","w")
+for i in fermate:
+    f.write(i+"\n")
+
+f=open("stazioni.csv","w")
+for i in stazioni:
+    f.write(i+"\n")
+
+f=open("viaggi.csv","w")
+for i in passaggio:
+    for j in i:
+        f.write(j+"\t")
+    f.write("\n")
