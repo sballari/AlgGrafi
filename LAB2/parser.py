706a0c0b984f29f856b7b7597b9e0271fdcfc5c7
RossiDaniel <bovo.96@gmail.com>
2019-03-22 09:26:53 +0100

    new parser

--- LAB2/parser.py
+++ LAB2/parser.py
@@ -3,19 +3,15 @@ from os.path import isfile, join
 import unicodedata
 import string
 
-mezzi = set()
-corse = set()
-fermate = set()
-stazioni = set()
-passaggio = []
+coppia = []
 
-mypath="/home/rossidaniel/Documenti/data_transport/"
+mypath="/home/rossidaniel/ownCloud/data_transport/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 for t_file in onlyfiles:
     f=file(str(mypath)+t_file)
     s=f.read()
-    s=''.join(x if x in string.printable else " " for x in s)
+    s=''.join(x if x in string.printable else "?" for x in s)
     s=s.split('*Z ')
     s.pop(0)
     for trip in s:
@@ -27,48 +23,31 @@ for t_file in onlyfiles:
         second_line=lines[0].split(" ")[:2]
         first_line[1]=first_line[1].replace("-","")+second_line[1]
 
-        corse.add(first_line[0])
-        mezzi.add(first_line[1])
+        corsa=first_line[0]
+        linea=first_line[1]
 
         while lines[0].split(" ")[0][0]=='*':
             lines.pop(0)
+        fermate=[]
         for line in lines:
-            fermata=line[0:9]
-            stazione=line[10:30]
+            cod_fermata=line[0:9]
+            #stazione=line[10:30]
             start=line[32:37]
             finish=line[39:44]
             
-            if fermata != '':
-                fermate.add(fermata)
-                stazioni.add(stazione)
-                passaggio.append([fermata,stazione,start,finish])      
-
-        corse.add(first_line[0])
-        mezzi.add(first_line[1])
-
+            if cod_fermata != '':
+                fermate.append([cod_fermata,start,finish])
+                if start != "     ":
+                    stazione1=fermate[len(fermate)-2]
+                    stazione2=fermate[len(fermate)-1]
+                    coppia.append([stazione1[0],stazione2[0],stazione1[2],stazione2[1],corsa,linea])
     f.close()
 
-for i in passaggio:
+for i in coppia:
     print i
 
-f=open("mezzi.csv","w")
-for i in mezzi:
-    f.write(i+"\n")
-
-f=open("corse.csv","w")
-for i in corse:
-    f.write(i+"\n")
-
-f=open("fermate.csv","w")
-for i in fermate:
-    f.write(i+"\n")
-
-f=open("stazioni.csv","w")
-for i in stazioni:
-    f.write(i+"\n")
-
-f=open("viaggi.csv","w")
-for i in passaggio:
+f=open("/home/rossidaniel/ownCloud/viaggi.csv","w")
+for i in coppia:
     for j in i:
         f.write(j+"\t")
     f.write("\n")
