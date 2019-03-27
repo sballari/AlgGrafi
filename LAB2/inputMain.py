d2fcc1aad010c9fdb40d61604b1af0696f30cbf0
sballari <ballarinsimone@gmail.com>
2019-03-27 18:33:00 +0100

    main desbestemiato

--- LAB2/inputMain.py
+++ LAB2/inputMain.py
@@ -4,11 +4,10 @@ import sys
  
 sys.setrecursionlimit(1000000)
 
-print "porcoddio"
+print "inizio lettura viaggi"
 G1 = graph.OrientedGraph.inputGraph("viaggi.csv")
 print "fine"
 
-for i in G1.nodes:
-    for j in G1.nodes[i].adj:
-        for k in G1.nodes[i].adj[j]:
-            print k
+roma = G1.nodes["200403018"]
+for key in roma.adj:
+    print 'dest:',key, 'corresponds to', roma.adj[key]
