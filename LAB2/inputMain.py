15df3c48836993ce2f3e42e3314a606edd133062
sballari <ballarinsimone@gmail.com>
2019-04-03 15:52:25 +0200

    calcolo distanze eseguito

--- LAB2/inputMain.py
+++ LAB2/inputMain.py
@@ -1,6 +1,8 @@
+import parserCoord
 from dijkstra import dijkstra
-from graph import OrientedGraph,Hour
+from graph import Hour, OrientedGraph
 
+"""
 #creo il grafo orientato
 G = OrientedGraph.inputGraph("viaggi.csv")
 
@@ -18,3 +20,13 @@ print i,[str(j) for j in data[i]]
 while parent[i]!=None:
     print parent[i],[str(j) for j in data[i]]
     i=parent[i]
+"""
+#########################
+### codice prova dist ###
+#########################
+coordMap = parserCoord.readCord()
+distanza = parserCoord.distance("200419036", "200802002", coordMap)
+print distanza
+
+# A: 200419036	logn lat ('6.121555', '49.616270',
+# B : 200802002	('6.135915', '49.671235')
--- LAB2/parserCoord.py
+++ LAB2/parserCoord.py
@@ -2,32 +2,54 @@ from os import listdir
 from os.path import isfile, join
 import unicodedata
 import string
+from math import radians, sin, cos, acos
 
-mypath="bfkoord"
 
-f=file(mypath)
-s=f.read()
-s=''.join(x if x in string.printable else "?" for x in s)
-s=s.split('\n')
-s=s[2:]
-s.pop(-1)
+def readCord():
+    #return: una mappa key:codStaz -> value:(long,lat)
+    mypath="bfkoord"
 
-bfkoord = dict() # key:codStaz -> value:(long,lat)
-for row in s:
-    data = row.split(" ")
-    data = filter(lambda r : r!="", data)
+    f=file(mypath)
+    s=f.read()
+    s=''.join(x if x in string.printable else "?" for x in s)
+    s=s.split('\n')
+    s=s[2:]
+    s.pop(-1)
 
-    codStaz=data[0]
-    longitud = data[1]
-    lat = data[2]
-    nameStaz = "".join(data[4:])
-    bfkoord[codStaz]=(longitud,lat,nameStaz)
+    bfkoord = dict() # key:codStaz -> value:(long,lat)
+    for row in s:
+        data = row.split(" ")
+        data = filter(lambda r : r!="", data)
+
+        codStaz=data[0]
+        longitud = data[1]
+        lat = data[2]
+        nameStaz = "".join(data[4:])
+        bfkoord[codStaz]=(longitud,lat,nameStaz)
+        
+    f.close()
+
+    f=open("coord.csv","w")
+    for i in bfkoord:
+        f.write(i)
+        f.write("\t")
+        f.write(str(bfkoord[i]))
+        f.write("\n")
+    f.close()
     
-f.close()
+    return bfkoord
+
+
+def distance(stazA, stazB, coordMap):
+    #approssimata per difetto
+    coordA = coordMap[stazA]
+    coordB = coordMap[stazB]
+    slat = radians(float(coordA[1]))
+    slon = radians(float(coordA[0]))
+    elat = radians(float(coordB[1]))
+    elon = radians(float(coordB[0]))
+    dist = dist = float(6371.01) * acos(sin(slat)*sin(elat) + cos(slat)*cos(elat)*cos(slon - elon))
+    return int(dist)
 
 
 
-f=open("coord.csv","w")
-for i in bfkoord:
-    f.write(i)
-    f.write("\n")
