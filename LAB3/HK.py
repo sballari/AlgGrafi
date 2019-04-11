829659de14a8ca03f6cf2a44009c1e8e8465cd6a
sballari <ballarinsimone@gmail.com>
2019-04-11 14:59:53 +0200

    eliminata condivisione di memoria accidentale

--- LAB3/HK.py
+++ LAB3/HK.py
@@ -1,21 +1,23 @@
 from graph import MatrixCompleteGraph
 
-def HKTSP(G) :
-    D = [dict()] * len(G.weight_matrix)
-    P = [dict()] * len(G.weight_matrix)
-    S = set(range(0,len(G.weight_matrix)))
-    ret = HKVisit(0,S,G,D,P)
+
+def HKTSP(G):
+    D = [dict() for i in range(len(G.weight_matrix))] 
+    P = [dict() for i in range(len(G.weight_matrix))] 
+    S = set(range(0, len(G.weight_matrix)))
+    ret = HKVisit(0, S, G, D, P)
     print D
     return ret
 
-def HKVisit(v,S,G,D,P) : 
+
+def HKVisit(v, S, G, D, P):
     # v : vertice di arrivo
     # G : grafo pesato delle distanze
     # S : vertici che devo visitare partendo da 0 n-1 -> e' un Set
-    
-    if len(S)==1 :
-            v_index = S.copy().pop()
-            return G.getweight(v_index,0)
+
+    if len(S) == 1:
+        v_index = S.copy().pop()
+        return G.getweight(v_index, 0)
     else:
         if D[v].get(str(S)) != None:
             return D[v][str(S)]
@@ -23,11 +25,10 @@ def HKVisit(v,S,G,D,P) :
         else:
             mindist = None
             minprec = None
-            S_=S.difference({v}) # S=S\{v}
-            
+            S_ = S.difference({v})  # S=S\{v}
+
             for u in S_:
-                print (u,S_)
-                dist=HKVisit(u,S_,G,D,P)+G.getweight(u,v)
+                dist = HKVisit(u, S_, G, D, P)+G.getweight(u, v)
                 if mindist == None:
                     mindist = dist
                     minprec = u
