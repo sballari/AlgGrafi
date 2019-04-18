a0b80afb74ed15cf46c6b89497d4eee68b68c2f9
sballari <ballarinsimone@gmail.com>
2019-04-18 17:18:19 +0200

    approssimazione con MST

--- LAB3/Main.py
+++ LAB3/Main.py
@@ -1,10 +1,15 @@
 from graph import MatrixCompleteGraph
 import HK
+import randomInsertion
 
 b14 = "burma14"
 u22 = "ulysses22"
 ds_name = b14
 
 G = MatrixCompleteGraph.createGraphFromTSP(ds_name+".tsp")
+
+circ = randomInsertion.RandomInsertion(G)
+print circ 
+print randomInsertion.weightCirc(circ,G)
 
 print HK.HKTSP(G)
